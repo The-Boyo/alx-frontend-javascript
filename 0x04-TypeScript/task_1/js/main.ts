81d95141ab838f19da8b93c33f49ff95de4ed895
Eliud Metto <eliudmett093@gmail.com>
2025-07-12 10:50:58 +0300

    Added a printTeacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,7 +1,7 @@
 interface Teacher {
 	readonly firstName: string;
 	readonly lastName: string;
-	fullTimeEmploy: boolean;
+	fullTimeEmployee: boolean;
 	yearsOfExperience?: number;
 	location: string;
 	[key: string]: any;
@@ -10,9 +10,36 @@ interface Teacher {
 const teacher1: Teacher = {
 	firstName: "Kimani",
 	lastName: "Rurii",
-	fullTimeEmploy: true,
+	fullTimeEmployee: true,
 	location: "Eldoret",
 	isMarried: true,
 };
 
 console.log(teacher1);
+
+/* Number 2 - Extending the Teacher class*/
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+const director1: Directors = {
+	firstName: "John",
+	lastName: "Doe",
+	location: "London",
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+console.log(director1);
+
+/* Number 3- Printing Teachers */
+
+interface PrintTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (a: string, b: string): string => {
+	return `${a.split("")[0]}. ${b}`;
+};
+
+console.log(printTeacher("Jacob", "Juma"));
