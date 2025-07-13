c2e9e56788bf29b99caa5ed0e1ad6b85f3178499
Eliud Metto <eliudmett093@gmail.com>
2025-07-13 09:26:27 +0300

    Completed task number 6 by implementing function specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -70,3 +70,19 @@ console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee("$500"));
 console.log(createEmployee("400"));
+
+/**Number 6- Functions Specific to employees */
+const isDirector = (employee: Director | Teacher): employee is Director => {
+	return (employee as Director).workDirectorTasks !== undefined;
+};
+
+const executeWork = (employee: Director | Teacher): string => {
+	if (isDirector(employee)) {
+		return employee.workDirectorTasks();
+	} else {
+		return employee.workTeacherTasks();
+	}
+};
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
