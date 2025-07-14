dc0a5ea47c56f4626cb9867ad0a063ebeac7c995
Eliud Metto <eliudmett093@gmail.com>
2025-07-14 22:55:43 +0300

    Worked pn task number 11 on brand convention and nominal typing

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,19 @@
+export const cpp = new Subjects.Cpp({
+	firstName: "",
+	lastName: "",
+	experienceTeachingC: 10,
+});
+
+export const java = new Subjects.Java({
+	firstName: "",
+	lastName: "",
+	experienceTeachingJava: 7,
+});
+
+export const react = new Subjects.React({
+	firstName: "",
+	lastName: "",
+	experienceTeachingReact: 3,
+});
+
+export const cTeacher = {};
